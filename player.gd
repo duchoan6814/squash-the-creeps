fac59d9cd58bbac8d5e62895bbaaa6b0409ae32a
Hoan Truong <hoantruong6814@gmail.com>
2023-05-06 15:38:41 +0700

    init project and handle player move

---
+++ player.gd
@@ -0,0 +1,34 @@
+extends CharacterBody3D
+
+# How fast the player moves in meters per second.
+@export var speed = 14
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 75
+
+var target_velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	
+	if(Input.is_action_pressed("move_back")):
+		direction.z +=  1
+	if(Input.is_action_pressed("move_forward")):
+		direction.z -=  1
+	if(Input.is_action_pressed("move_left")):
+		direction.x -=  1
+	if(Input.is_action_pressed("move_right")):
+		direction.x += 1
+		
+	if(direction != Vector3.ZERO):
+		direction = direction.normalized()
+		$Pivot.look_at(position + direction, Vector3.UP)
+	
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	
+	if not is_on_floor():
+		target_velocity.y -= fall_acceleration * delta
+		
+	# move the charactor
+	velocity = target_velocity
+	move_and_slide()
